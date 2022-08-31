e16062b0572160b64a978fab99e2edf1e3a8c5c0
Bartkooo <bartosz.plochocki@gmail.com>
2022-08-31 14:24:30 +0200

    selling items

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
 
 const items = require('./items.json')
 
@@ -10,9 +12,21 @@ app.get('/', (req, res) => {
   res.send(items)
 })
 
-app.get('/item/:id', (req,res) => {
-  const item = items.find(item => item.id == req.params.id)
+app.get('/item/:id', (req, res) => {
+  const item = items.find((item) => item.id == req.params.id)
   res.send(item)
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/sell', (req, res) => {
+  items.push(req.body)
+  let json = JSON.stringify(items)
+  fs.writeFile('items.json', json, function (err) {
+    if (err) throw err
+  })
+  res.send('')
 })
 
 app.listen(4000)
